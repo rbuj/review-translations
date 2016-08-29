5748f0baf2c25f86f5f260e6a126e8a93c40797e
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-29 22:13:23 +0200

    refactor shared-mime-info

--- shared-mime-info.sh
+++ shared-mime-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ---------------------------------------------------------------------------
-# Copyright 2015, Robert Buj <rbuj@fedoraproject.org>
+# Copyright 2016, Robert Buj <rbuj@fedoraproject.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,8 +17,9 @@ RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 NC=`tput sgr0` # No Color
 
+PROJECT_NAME=shared-mime-info
 WORK_PATH=$PWD
-BASE_PATH=${WORK_PATH}
+LIST=${WORK_PATH}/list/${PROJECT_NAME}.list
 
 LANG_CODE=
 GENERATE_REPORT=
@@ -26,137 +27,16 @@ DISABLE_WORDLIST=
 INSTALL_TRANS=
 
 function usage {
-    echo "This script downloads the translation of shared-mime-info"
+    echo "This script downloads the translation of ${PROJECT_NAME}"
     echo "    usage : $0 -l|--lang=LANG_CODE [ARGS]"
     echo -ne "\nMandatory arguments:\n"
     echo "   -l|--lang=LANG_CODE   Locale to pull from the server"
     echo -ne "\nOptional arguments:\n"
     echo "   -r, --report          Generate group report"
     echo "   --disable-wordlist    Do not use wordlist file"
+    echo "   -i, --install         Install translations"
     echo "   -h, --help            Display this help and exit"
     echo ""
-}
-
-function download_code {
-    cd ${BASE_PATH}
-    if [ ! -d "${1}" ]; then
-        echo -ne "git clone "
-        git clone $2 $1 &> /dev/null && echo "${GREEN}[ OK ]${NC}" || echo "${RED}[ FAIL ]${NC}"
-    else
-        cd $1
-        echo -ne "git pull "
-        git pull &> /dev/null && echo "${GREEN}[ OK ]${NC}" || echo "${RED}[ FAIL ]${NC}"
-    fi
-}
-
-function get_code {
-    download_code shared-mime-info http://anongit.freedesktop.org/git/xdg/shared-mime-info.git
-}
-
-function get_trans {
-    rpm -q transifex-client &> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "report : installing required packages"
-        set -x
-        sudo dnf install -y transifex-client
-        set -
-    fi
-    echo -ne "downloading translation "
-    cd ${BASE_PATH}/shared-mime-info
-    rm -f po/${LANG_CODE}.po
-    tx pull -l ${LANG_CODE} > /dev/null
-    if [ $? -ne 0 ]; then
-        echo " ${RED}[ FAIL ]${NC}"
-        exit 1
-    else
-        echo " ${GREEN}[ OK ]${NC}"
-    fi
-}
-
-function download {
-    get_code
-    get_trans
-}
-
-function fedora_wordlist {
-    DICT=${WORK_PATH}/pology/lang/${LANG_CODE}/spell/report-fedora.aspell
-    if [ -n "${DISABLE_WORDLIST}" ]; then
-        if [ -f "${DICT}" ]; then
-            rm -f ${DICT}
-        fi
-    else
-        if [ ! -d "${WORK_PATH}/pology/lang/${LANG_CODE}/spell" ]; then
-            mkdir -p ${WORK_PATH}/pology/lang/${LANG_CODE}/spell
-        fi
-        WORDS=`cat ${WORK_PATH}/wordlist | wc -l`
-        echo "personal_ws-1.1 ${LANG_CODE} ${WORDS} utf-8" > ${DICT}
-        cat ${WORK_PATH}/wordlist >> ${DICT}
-    fi
-}
-
-function report {
-    rpm -q aspell-${LANG_CODE} python-enchant enchant-aspell &> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "report : installing required packages"
-        set -x
-        sudo dnf install -y aspell-${LANG_CODE} python-enchant enchant-aspell
-        set -
-    fi
-    #########################################
-    # LANGUAGETOOL
-    #########################################
-    if [ ! -d "${WORK_PATH}/languagetool" ]; then
-        ${WORK_PATH}/common/build-languagetool.sh --path=${WORK_PATH} -l=${LANG_CODE}
-    fi
-    cd ${WORK_PATH}
-    LANGUAGETOOL=`find . -name 'languagetool-server.jar'`
-    java -cp $LANGUAGETOOL org.languagetool.server.HTTPServer --port 8081 > /dev/null &
-    LANGUAGETOOL_PID=$!
-
-    echo -ne "report : waiting for langtool"
-    until $(curl --output /dev/null --silent --data "language=ca&text=Hola món!" --fail http://localhost:8081); do
-        printf '.'
-        sleep 1
-    done
-    if [ $? -ne 0 ]; then
-        echo " ${RED}[ FAIL ]${NC}"
-    else
-        echo " ${GREEN}[ OK ]${NC}"
-    fi
-
-    #########################################
-    # POLOGY
-    #########################################
-    if [ ! -d "${WORK_PATH}/pology" ]; then
-        ${WORK_PATH}/common/build-pology.sh --path=${WORK_PATH}
-    fi
-    export PYTHONPATH=${WORK_PATH}/pology:$PYTHONPATH
-    export PATH=${WORK_PATH}/pology/bin:$PATH
-    fedora_wordlist
-
-    HTML_REPORT=${WORK_PATH}/shared-mime-info-report.html
-    cat << EOF > ${HTML_REPORT}
-<!DOCTYPE html>
-<html lang="${LANG_CODE}" xml:lang="${LANG_CODE}" xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <title>Translation Report</title>
-  </head>
-<body bgcolor="#080808" text="#D0D0D0">
-EOF
-
-    echo "************************************************"
-    echo "* checking translation..."
-    echo "************************************************"
-    posieve check-rules,check-spell-ec,check-grammar,stats -s lang:${LANG_CODE} -s showfmsg -s byrule --msgfmt-check --skip-obsolete --coloring-type=html ${BASE_PATH}/shared-mime-info/po/${LANG_CODE}.po >> ${HTML_REPORT}
-
-    cat << EOF >> ${HTML_REPORT}
-</body>
-</html>
-EOF
-
-    chmod 644 ${HTML_REPORT}
-    kill -9 ${LANGUAGETOOL_PID} > /dev/null
 }
 
 for i in "$@"
@@ -171,6 +51,9 @@ case $i in
     ;;
     --disable-wordlist)
     DISABLE_WORDLIST="YES"
+    ;;
+    -i|--install)
+    INSTALL_TRANS="YES"
     ;;
     -h|--help)
     usage
@@ -193,8 +76,14 @@ if [ -z "${GENERATE_REPORT}" ] && [ -n "${DISABLE_WORDLIST}" ]; then
 fi
 
 ### Main
-download
 if [ -n "$GENERATE_REPORT" ]; then
-    report
+    if [ "$DISABLE_WORDLIST" == "YES" ]; then
+        ./common/transifex-translations.sh -r -l=${LANG_CODE} --disable-wordlist -p=${PROJECT_NAME} -f=${LIST} -w=${WORK_PATH}
+    else
+        ./common/transifex-translations.sh -r -l=${LANG_CODE} -p=${PROJECT_NAME} -f=${LIST} -w=${WORK_PATH}
+    fi
+fi
+if [ -n "$INSTALL_TRANS" ]; then
+    ./common/transifex-translations.sh -i -l=${LANG_CODE} -p=${PROJECT_NAME} -f=${LIST} -w=${WORK_PATH}
 fi
 echo "complete!"
