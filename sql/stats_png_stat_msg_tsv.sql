7ae85fe8819ab095500a8c5d8690917164656b8f
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-09-08 10:22:54 +0200

    stats: hide locales with 0 translated messages/words

--- sql/stats_png_stat_msg_tsv.sql
+++ sql/stats_png_stat_msg_tsv.sql
@@ -4,6 +4,16 @@ SELECT t_locales.name, t_states.name, SUM(t_stats.msg)
         INNER JOIN t_locales ON t_updates.id_locale = t_locales.id
         INNER JOIN t_components ON t_updates.id_component = t_components.id
         INNER JOIN t_states ON t_stats.id_state = t_states.id
-    WHERE t_updates.active = 1
-   GROUP BY t_states.name, t_locales.name
-   ORDER BY t_locales.name;
+    WHERE
+	t_updates.active = 1 AND
+        t_locales.name IN (
+            SELECT DISTINCT t_locales.name
+            FROM t_stats
+                INNER JOIN t_updates ON t_stats.id_update = t_updates.id
+                INNER JOIN t_locales ON t_updates.id_locale = t_locales.id
+                INNER JOIN t_components ON t_updates.id_component = t_components.id
+                INNER JOIN t_states ON t_stats.id_state = t_states.id
+            WHERE t_updates.active = 1 AND t_stats.msg > 0 AND t_states.name = 'translated'
+        )
+    GROUP BY t_states.name, t_locales.name
+    ORDER BY t_locales.name;
