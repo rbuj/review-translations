24441dfab699b1064a8e3374f0f7ad6d540f0e6f
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-09-10 10:35:42 +0200

    symbolic links

---
+++ project.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Copyright 2015, Robert Buj <rbuj@fedoraproject.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License at <http://www.gnu.org/licenses/> for
+# more details.
+# ---------------------------------------------------------------------------
+source ${PWD}/conf/$(basename ${0} .sh).conf
+
+${PWD}/common/translations.sh -p=${PROJECT_NAME} -f=${LIST} -w=${WORK_PATH} -t=${TYPE} -d=${DOCUMENT} $@
