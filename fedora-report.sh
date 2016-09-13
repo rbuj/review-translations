a89787608ef508638ee42dde520728f28cb88bca
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-09-13 20:01:13 +0200

    fedora-report.sh: remove DOCTYPE svg PUBLIC

--- fedora-report.sh
+++ fedora-report.sh
@@ -237,6 +237,8 @@ EOF
 EOF
         fi
     done
+    sed -i "/DOCTYPE svg PUBLIC/d" ${HTML_REPORT}
+    sed -i "/http\:\/\/www\.w3\.org\/Graphics\/SVG\/1\.1\/DTD\/svg11\.dt/d" ${HTML_REPORT}
 }
 
 ########################################################
