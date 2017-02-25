4258f31b2e2044e9fa7b2c18eefb8982c47f0aaa
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-02-25 09:01:20 +0100

    common/zanata.sh: sed replace url, use local vles

--- common/zanata.sh
+++ common/zanata.sh
@@ -14,17 +14,22 @@
 # more details.
 # ---------------------------------------------------------------------------
 function project_folder {
-    if [ ! -d "${BASE_PATH}/${1}-${2}" ]; then
-        if [ -n "${VERBOSE}" ]; then echo -ne "${1} (${2}) : creating project folder "; fi
-       	mkdir -p ${BASE_PATH}/${1}-${2} > /dev/null && if [ -n "${VERBOSE}" ]; then echo "${GREEN}[ OK ]${NC}"; fi || exit 1
+    local PROJECT=$1
+    local VERSION=$2
+    local PRJ_FOLDER=${BASE_PATH}/${PROJECT}-${VERSION}
+    if [ ! -d "${PRJ_FOLDER}" ]; then
+        if [ -n "${VERBOSE}" ]; then echo -ne "${PROJECT} (${VERSION}) : creating project folder "; fi
+        mkdir -p ${PRJ_FOLDER} > /dev/null && if [ -n "${VERBOSE}" ]; then echo "${GREEN}[ OK ]${NC}"; fi || exit 1
     fi
 }
 
 function project_config {
-    local ZANATA_FILE=${BASE_PATH}/${1}-${2}/zanata.xml
+    local PROJECT=$1
+    local VERSION=$2
+    local ZANATA_FILE=${BASE_PATH}/${PROJECT}-${VERSION}/zanata.xml
     if [ ! -f "${ZANATA_FILE}" ]; then
         if [ -n "${VERBOSE}" ]; then echo -ne "${1} (${2}) : creating zanata.xml file "; fi
-        sed "s/URL/$BASE_URL/g;s/PROJECT/$1/g;s/VERSION/$2/g" ${WORK_PATH}/snippet/zanata.xml > ${ZANATA_FILE}
+        sed "s/URL/${BASE_URL//\//\\\/}/g;s/PROJECT/$PROJECT/g;s/VERSION/$VERSION/g" ${WORK_PATH}/snippet/zanata.xml > ${ZANATA_FILE}
         if [ $? -ne 0 ]; then
             if [ -n "${VERBOSE}" ]; then echo "${RED}[ FAIL ]${NC}"; fi
         else
@@ -34,8 +39,11 @@ function project_config {
 }
 
 function project_download {
+    local PROJECT=$1
+    local VERSION=$2
+    local PRJ_FOLDER=${BASE_PATH}/${PROJECT}-${VERSION}
     if [ -n "${VERBOSE}" ]; then echo -ne "${1} (${2}) : downloading project translation "; fi
-    cd ${BASE_PATH}/${1}-${2}
+    cd ${PRJ_FOLDER}
     if [ -z "${ALL_LANGS}" ]; then
         zanata-cli -B -q pull -l ${LANG_CODE} --pull-type trans > /dev/null && echo "${GREEN}[ OK ]${NC}" || echo "${RED}[ FAIL ]${NC}";
     else
