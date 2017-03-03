aca6d002012ad62f88023dad74d28c6370e6f7bf
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-03-03 09:46:09 +0100

    fix parse of rule id

--- snippet/parse-grammar.pl
+++ snippet/parse-grammar.pl
@@ -8,24 +8,24 @@ while (<STDIN>) {
     print "<item>";
   } elsif (m/^<b>.*<\/b><br\/>/) {
     my $file = $_;
-    $file =~ s/^<b>(.*)<\/b><br\/>\n/$1/g;
+    $file =~ s/^<b>(.*)<\/b><br\/>\n/$1/;
     print "<file>$file</file>";
   } elsif (m/^<b>Context\:.*/) {
     my $context = $_;
-    $context =~ s/^<b>Context\:<\/b>[\s]*(.*)<br\/>\n/$1/g;
+    $context =~ s/^<b>Context\:<\/b>[\s]*(.*)<br\/>\n/$1/;
     print "<context>$context</context>";
   } elsif (m/^\(.*\)\s+.*<br\/>\n/) {
     my $rule = $_;
-    $rule =~ s/^\((.*)\).*\n/$1/g;
+    $rule =~ s/^\(([[A-Z0-9_]+)\)\s.*\n/$1/;
     print "<rule>$rule</rule>";
     my $tip = $_;
-    $tip =~ s/^.*<\/font><\/b>(.*)<br\/>\n/$1/g;
+    $tip =~ s/^.*<\/font><\/b>(.*)<br\/>\n/$1/;
     print "<tip>$tip";
   } elsif (m/^\s+<br\/>\n/) {
      ;
   } elsif (m/^\s+.*<br\/>\n/) {
     my $line = $_;
-    $line =~ s/^\s+(.*)<br\/>\n/$1/g;
+    $line =~ s/^\s+(.*)<br\/>\n/$1/;
     print "<br/>$line";
   } else {
     print;
