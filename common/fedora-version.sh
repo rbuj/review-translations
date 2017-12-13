eec91f59cbb0a8599167d575535309fb808f1c6a
rbuj <robert.buj@gmail.com>
2017-12-13 13:19:57 +0100

    fix fedora-version.sh

--- common/fedora-version.sh
+++ common/fedora-version.sh
@@ -17,10 +17,10 @@ VERSION="master"
 if [ -f "/etc/fedora-release" ]; then
     VERSION_AUX=`cat /etc/fedora-release`
     case ${VERSION_AUX} in
-	"Fedora release 25 (Twenty Six)")
+	"Fedora release 26 (Twenty Six)")
         VERSION="f26"
         ;;
-        "Fedora release 26 (Twenty Seven)")
+        "Fedora release 27 (Twenty Seven)")
         VERSION="f27"
         ;;
     esac
